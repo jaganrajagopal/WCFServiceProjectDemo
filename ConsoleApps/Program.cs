f4c5e12c5cb1105cb2e6459adb18d094c71819b5
Jagan <jagan.rajagopal@outlook.com>
2019-02-07 18:02:33 +0530

    WCF project demo

---
+++ ConsoleApps/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost myHost = new ServiceHost(typeof(WCFService.Service1)))
+            {
+                myHost.Open();
+                Console.WriteLine("Started at this point"+ DateTime.Now.ToString());
+                Console.ReadLine();
+               // myHost.Close();
+            }
+        }
+    }
+}
---
+++ WCFClients/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WCFClients
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btname_Click(object sender, EventArgs e)
+        {
+            MyWCFservice.Service1Client objclient = new MyWCFservice.Service1Client("BasicHttpBinding_IService1");
+           string  result= objclient.GetData(1);
+
+        }
+    }
+}
